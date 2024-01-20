17c4368db3cfbda76f10bbc456eb1b0582ac0b7d
alepaillas <alepaillas@gmail.com>
2024-01-20 14:30:34 -0300

    añadí logo y favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from "./components/NavBar/NavBar";
+import logo from "./assets/img/circle-black-text-512.webp"
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Galería Anfisbena</h1>
-      </header>
-      <NavBar />
-      <ItemListContainer greeting={'Bienvenidos'}/>
-    </div>
-  );
+    return (
+	<div className="App">
+	    <header>
+		<img src={logo} alt="Logo"></img>
+		<h1>Galería Anfisbena</h1>
+	    </header>
+	    <NavBar />
+	    <ItemListContainer greeting={'Bienvenidos'}/>
+	</div>
+    );
 }
 
 export default App;
