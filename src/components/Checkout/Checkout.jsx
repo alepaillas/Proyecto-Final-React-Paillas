024d9cc41f937ac455076ae36439882f2f52cb12
alepaillas <alepaillas@gmail.com>
2024-02-26 23:11:13 -0300

    valida el email usado en el checkout usando 2 inputs

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,9 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
+import "./Checkout.css";
 
 const Checkout = () => {
   const { cart, totalValue } = useContext(CartContext);
+
+  // estados para el form
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    confirmEmail: "",
+    phone: "",
+    address: "",
+  });
+
+  // usamos la propiedad name para identificar el input que queremos setear
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // llenamos con la data previa + nuestro input modificado
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const [showConfirmEmailError, setShowConfirmEmailError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    formData.email == formData.confirmEmail
+      ? (console.log(formData), setShowConfirmEmailError(false))
+      : (console.error("Confirme su email"), setShowConfirmEmailError(true));
+  };
 
   return (
     <>
@@ -27,34 +53,86 @@ const Checkout = () => {
         </p>
       </div>
       <div className="row">
-        <form className="col s6 offset-s3">
+        <form
+          onSubmit={handleSubmit}
+          className="col s6 offset-s3 clickThroughLabels"
+        >
           <div className="input-field">
             <i className="material-icons prefix">account_circle</i>
-            <input id="name" type="text" className="validate"></input>
+            <input
+              name="name"
+              type="text"
+              value={formData.name}
+              onChange={handleChange}
+              className="validate"
+            ></input>
             <label htmlFor="name">Nombre y Apellido</label>
           </div>
           <div className="input-field">
             <i className="material-icons prefix">email</i>
-            <input id="email" type="email" className="validate"></input>
+            <input
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="validate"
+            ></input>
             <label htmlFor="email">E-mail</label>
           </div>
           <div className="input-field">
+            <i className="material-icons prefix">email</i>
+            <input
+              name="confirmEmail"
+              type="email"
+              value={formData.confirmEmail}
+              onChange={handleChange}
+              // clase condicional en caso de error
+              className={
+                showConfirmEmailError == true
+                  ? "validate invalid"
+                  : "validate valid"
+              }
+            ></input>
+            <label htmlFor="confirmEmail">Repita su E-mail</label>
+            {/* helper text condicional en caso de error */}
+            {showConfirmEmailError && (
+              <span
+                className="helper-text"
+                data-error="Confirme que su E-mail este bien escrito"
+              ></span>
+            )}
+          </div>
+          <div className="input-field">
             <i className="material-icons prefix">phone</i>
-            <input id="phone" type="tel" className="validate"></input>
+            <input
+              name="phone"
+              type="tel"
+              value={formData.phone}
+              onChange={handleChange}
+              className="validate"
+            ></input>
             <label htmlFor="phone">Teléfono (+56912345678)</label>
           </div>
           <div className="input-field">
             <i className="material-icons prefix">location_on</i>
-            <input id="address" type="text" className="validate"></input>
+            <input
+              name="address"
+              type="text"
+              value={formData.address}
+              onChange={handleChange}
+              className="validate"
+            ></input>
             <label htmlFor="address">Mi calle 1234, Mi comuna, Mi región</label>
           </div>
+          <button type="submit" className="btn gridAutoMargin">
+            Finalizar compra
+          </button>
         </form>
       </div>
       <div className="center">
-        <Link to="/cart" className="btn marginHorizontal1">
+        <Link to="/cart" className="btn-small marginHorizontal1">
           Volver
         </Link>
-        <button className="btn marginHorizontal1">Finalizar compra</button>
       </div>
     </>
   );
